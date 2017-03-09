18cab492987b2378922447edff95276b2cb553cd
VonWebEinstein <vonweb@163.com>
2017-03-09 18:44:12 +0800

    mod events -- 'list'

--- R/events.R
+++ R/events.R
@@ -3,5 +3,5 @@
 eventsHierarchy <- function(species = "9606"){
   url = str_c("http://www.reactome.org/ContentService/data/eventsHierarchy/", species)
   str = GET(url, accept_json())
-  return(content(str))
+  return(list(list(children = content(str))))
 }
