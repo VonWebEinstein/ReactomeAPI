f5db657669cc162ab07e716c8df671df229cf62c
VonWebEinstein <vonweb@163.com>
2017-03-17 00:05:45 +0800

    add url2dataframe

---
+++ R/url2dataframe.R
@@ -0,0 +1,25 @@
+
+#' @include error.R
+#' @import jsonlite
+#' @import httr
+#' @import stringr
+#'
+url2dataframe <- function(urlComponent,
+                          bindToSingleDataframe = TRUE,
+                          silent = FALSE){
+
+  url = str_c(urlComponent, collapse = '/')
+  res = try(fromJSON(url), silent = TRUE)
+  if(inherits(res, "try-error")){
+    tmp = content(GET(url, content_type_json()))
+    return(errorMessage(tmp, silent = silent))
+  }
+  else{
+    # bind dataframes to single one
+    if(bindToSingleDataframe && !is.data.frame(res)){
+      res = do.call(rbind, res)
+    }
+    return(res)
+  }
+
+}
