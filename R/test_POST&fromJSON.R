8247ec05db700583b5480808f92c3b52b8fdcddb
liliangamss <dodo_1315@163.com>
2017-03-17 21:39:02 +0800

    some test

---
+++ R/test_POST&fromJSON.R
@@ -0,0 +1,18 @@
+# test_interactors
+
+res = fromJSON("http://www.reactome.org/ContentService/interactors/psicquic/molecule/MINT/Q13501/details", flatten = TRUE)
+
+res$entities[ ,c("acc", "count")]
+
+res1 = GET("http://www.reactome.org/ContentService/interactors/psicquic/molecule/MINT/Q13501/details", accept_json())
+
+cont1 = fromJSON(content(res1, as = "text"))
+
+url="http://www.reactome.org/ContentService/interactors/psicquic/molecules/MINT/details"
+
+res2 = POST(url, body="Q13501",content_type("text/plain"))
+
+content(res2)
+
+cont2 = fromJSON(content(res2, as = "text"))
+
