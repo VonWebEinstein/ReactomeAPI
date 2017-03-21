fdd850764c27b02bab80ee733c5890a9d45fe6cc
VonWebEinstein <vonweb@163.com>
2017-03-22 00:09:27 +0800

    update POST_Method, disease

--- R/reactomeDiseases.R
+++ R/reactomeDiseases.R
@@ -19,29 +19,44 @@
 #'
 #'
 #'
-reactomeDiseases <- function(DOID_only = FALSE) {
+reactomeDiseases <- function(DOID_only = FALSE){
   url = 'http://www.reactome.org/ContentService/data/diseases'
-  if(!DOID_only)
-    res = fromJSON(url)
+  if(DOID_only){
+    url = str_c(url, '/doid')
+    str = rawToChar(GET(url)$content)
+    str = str_split(str, '[\\n|\\t]')
+    dt = as.data.frame(t(matrix(str[[1]], nrow = 2)), stringsAsFactors = FALSE)
+    colnames(dt) = c('ID', 'DOID')
+
+    return(dt)
+  }
   else{
-    tmp = strsplit(as.character(content(GET(str_c(url,"/doid")))),"\n")
-    tmp = strsplit(tmp[[1]], '\t')
-    l = length(tmp)
-    tmp1 = list(1:l)
-    tmp2 = list(1:l)
-    # for(i in 1:l){
-    #     tmp1[[1]][i] = strsplit(as.character(tmp[[1]][i]),"\t")[[1]][1]
-    #     tmp2[[1]][i] = strsplit(as.character(tmp[[1]][i]),"\t")[[1]][2]
-    #   }
-    # tmp3 = list(1:l)
-    # for(i in 1:l){
-    #     tmp3[[1]][i] = strsplit(as.character(tmp2[[1]][i]),":")[[1]][2]
-    #   }
-    for(i in 1:l){
-      tmp1[[1]][i] = tmp[[i]][1]
-      tmp2[[1]][i] = tmp[[i]][2]
-    }
-    res = data.frame(tmp1[[1]],tmp2[[1]],stringsAsFactors = F)
-    return(res)
+    return(fromJSON(url))
   }
 }
+# reactomeDiseases <- function(DOID_only = FALSE) {
+#   url = 'http://www.reactome.org/ContentService/data/diseases'
+#   if(!DOID_only)
+#     res = fromJSON(url)
+#   else{
+#     tmp = strsplit(as.character(content(GET(str_c(url,"/doid")))),"\n")
+#     tmp = strsplit(tmp[[1]], '\t')
+#     l = length(tmp)
+#     tmp1 = list(1:l)
+#     tmp2 = list(1:l)
+#     # for(i in 1:l){
+#     #     tmp1[[1]][i] = strsplit(as.character(tmp[[1]][i]),"\t")[[1]][1]
+#     #     tmp2[[1]][i] = strsplit(as.character(tmp[[1]][i]),"\t")[[1]][2]
+#     #   }
+#     # tmp3 = list(1:l)
+#     # for(i in 1:l){
+#     #     tmp3[[1]][i] = strsplit(as.character(tmp2[[1]][i]),":")[[1]][2]
+#     #   }
+#     for(i in 1:l){
+#       tmp1[[1]][i] = tmp[[i]][1]
+#       tmp2[[1]][i] = tmp[[i]][2]
+#     }
+#     res = data.frame(tmp1[[1]],tmp2[[1]],stringsAsFactors = F)
+#     return(res)
+#   }
+# }
