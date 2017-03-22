45af5423a68f76f781abc4b942f8fa72421ed2d8
sumiaomiao1hao <sumiaomiao@sumiaomiaodeMacBook-Pro.local>
2017-03-23 00:41:31 +0800

    chang the argument

--- R/rtSchema.R
+++ R/rtSchema.R
@@ -8,15 +8,16 @@
 #' Or retrieves the dataframe of simplified reference objects that belong
 #' to the specified schema class.
 #'
-#' @param classname string. Schema class name
+#' @param schemaclass string. Schema class name.
 #' @param species string. Allowed species filter: SpeciesName
-#' (eg: Homo sapiens) SpeciesTaxId (eg: 9606)
+#' (eg: Homo sapiens) SpeciesTaxId (eg: 9606).
+#' @param specified string. "detail", "simplified", "count" or "reference".
 #' @param Page integer. Page to be returned.
 #' @param offset integer. Number of rows returned.
 #'
 #' @return dataframe or number.
 #'
-#' @examples schema_class = rtSchema(className = "pathway",species = "9606",page = 1,offset = 25,schema = FALSE)
+#' @examples schema_class = rtSchema(schemaclass = "pathway",species = "9606",specified = "detail",page = 1,offset = 25,silent = FALSE)
 #'
 #' @include error.R
 #' @import jsonlite
@@ -26,25 +27,21 @@
 #' @rdname schema
 #'
 
-reactomeSchema <- function(className = "Pathway",
+reactomeSchema <- function(schemaclass = "Pathway",
                            species = "9606",
+                           specified = "detail", # detail simplified count or reference
                            page = 1,
                            offset = 25,
-                           schema = FALSE,
-                           class = "count",
                            silent = FALSE){
-  url = 'http://www.reactome.org/ContentService/data/schema/'
-  if(!schema){
-    tmp_url = switch(class,
-                     count = str_c(url,className,"/",class,"?","species=",species),
-                     min = str_c(url,className,"/",class,"?","species=",species,"&","page=",page,"&","offset=",offset),
-                     reference = str_c(url,className,"/",class,"?","page=",page,"&","offset=",offset))
-  }
-  else
-    tmp_url = str_c(url,className,"?","species=",species,"&","page=",page,"&","offset=",offset)
-  res = try(fromJSON(tmp_url), silent = TRUE)
+  tmp_url = 'http://www.reactome.org/ContentService/data/schema/'
+  url = switch(specified,
+                detail = str_c(tmp_url,schemaclass,"?","species=",species,"&","page=",page,"&","offset=",offset),
+                simplified = str_c(tmp_url,schemaclass,"/","min","?","species=",species,"&","page=",page,"&","offset=",offset),
+                count = str_c(tmp_url,className,"/","count","?","species=",species),
+                reference = str_c(tmp_url,className,"/","reference","?","page=",page,"&","offset=",offset))
+  res = try(fromJSON(url), silent = TRUE)
   if(inherits(res, "try-error")){
-    tmp = content(GET(tmp_url, content_type_json()))
+    tmp = content(GET(url, content_type_json()))
     return(errorMessage(tmp, silent = silent))
   }
   else{
@@ -56,20 +53,18 @@ reactomeSchema <- function(className = "Pathway",
 #' @export
 #' @rdname schema
 
-rtSchema <- function(className = "Pathway",
+rtSchema <- function(schemaclass = "Pathway",
                      species = "9606",
+                     specified = "detail", # detail simplified count or reference
                      page = 1,
                      offset = 25,
-                     schema = FALSE,
-                     class = "count",
                      silent = FALSE){
-  return(reactomeSchema(className,
-                        species,
-                        page,
-                        offset,
-                        schema,
-                        class,
-                        silent))
+  return(reactomeSchema(schemaclass = schemaclass,
+                        species = specified,
+                        specified = specified,
+                        page = page,
+                        offset = offset,
+                        silent = silent))
 }
 
 
