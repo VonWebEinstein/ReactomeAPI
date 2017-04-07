7b22cd69d4ed427ae3ffd5a6b0893f3b92884de9
sumiaomiao1hao <sumiaomiao@sumiaomiaodeMacBook-Pro.local>
2017-04-07 11:13:09 +0800

    add some function,updating process

--- R/rtSchema.R
+++ R/rtSchema.R
@@ -1,23 +1,30 @@
 #' Schema class queries
 #'
-#' This method retrieves the dataframe of entries in Reactome
-#' that belong to the specified schema class. Or retrieves
-#' counts the total number of entries in Reactome that belong to
-#' the specified schema class. Or retrieves the dataframe of simplified
-#' entries in Reactome that belong to the specified schema class.
-#' Or retrieves the dataframe of simplified reference objects that belong
-#' to the specified schema class.
+#' This method retrieves the dataframe of specified interval of entries in Reactome
+#' that belong to the specified schema class.
 #'
 #' @param schemaclass string. Schema class name.
 #' @param species string. Allowed species filter: SpeciesName
 #' (eg: Homo sapiens) SpeciesTaxId (eg: 9606).
-#' @param specified string. "detail", "simplified", "count" or "reference".
-#' @param Page integer. Page to be returned.
-#' @param offset integer. Number of rows returned.
+#' @param specified string. "detail", "summary", "count" or "reference".
+#' @param intervalOFpathway vector of integer. Allow users to specify the interval of pathway.
+#' \code{intervalOFpathway = c(-1, 1)} return all results.
 #'
+#' @details
+#' \itemize{
+#'   \item\code{detail}: Return a dataframe of entries corresponding to a given schema class.
+#'   \item\code{summary}: Return a dataframe of simplified entries corresponding to a given schema class.
+#'   \item\code{count}: Return the number of entries belonging to the specified schema class.
+#'   \item\code{reference}: Return a dataframe of simplified reference objects corresponding to a given schema class.
+#'
+#' }
 #' @return dataframe or number.
 #'
-#' @examples schema_class = rtSchema(schemaclass = "Pathway",species = "9606",specified = "detail",page = 1,offset = 25,silent = FALSE)
+#' @examples
+#' schema_class = reactomeSchema(schemaclass = "Pathway",species = "9606",
+#'                                      type = "detail",intervalOFpathway = c(1, 10),silent = FALSE)
+#' schema_class = reactomeSchema(schemaclass = "Pathway",species = "Homo sapiens",
+#'                                      type = "detail",intervalOFpathway = c(-1, 1),silent = FALSE)
 #'
 #' @include url2dataframe.R
 #' @include error.R
@@ -33,11 +40,12 @@ reactomeSchema <- function(schemaclass = "Pathway",
                            type = "detail", # detail summary count or reference
                            intervalOFpathway = c(1,10),
                            silent = FALSE){
-  number = dataframeResPage(type = 'count')
   #let offset = 25 , calculate page under interval of pathway
 
   # # let user choose tocontinue or not
   if((intervalOFpathway[1] == 1)&(intervalOFpathway[2] == -1)){
+    number = dataframeResPage(type = 'count')
+    cat(str_c('The total number of pathway is : ', as.character(number)))
     return(schema(interval = c(1, number)))
 
   #   tmp = str_c("The count of ",
@@ -62,22 +70,27 @@ schema <- function(interval){
   page = lapply(interval, function(x) (floor((x-1) / 25) + 1))
   dataframeRes = lapply(page[[1]]:page[[2]],
                         function(x) dataframeResPage(page = x, total_pages = page[[2]]))
+
+  # check length of the return result in every node
   KEYS = unique(unlist(sapply(dataframeRes, names)))
-  if(!all(sapply(dataframeRes, length) == length(dataframeRes))){
+  if(!all(sapply(dataframeRes, length) == length(KEYS))){
     for(n in 1:length(dataframeRes)){
       missingNames = KEYS[!(KEYS %in% names(dataframeRes[[n]]))]
       dataframeRes[[n]][missingNames] = NA
     }
   }
+
+  # combine all results that users specify
   res = do.call(rbind, dataframeRes)
   l = interval[1]
   r = interval[2]
-  return(res[l:r,])
+  res = res[l:r,]
+  return(res)
 }
 
 
 
-#  the dataframe response page = x
+#  the dataframe response page = x, cat the updating of code
 dataframeResPage <- function(schemaclass = 'Pathway',
                              species = "9606",
                              type = 'detail', # detail summary count or reference
@@ -106,11 +119,15 @@ dataframeResPage <- function(schemaclass = 'Pathway',
                  reference = str_c(tmp_url,reference_url))
     # 最好显式返回
     res = url2dataframe(urlComponent = url)
+
+    # cat the updating rely on the page
     cat(printRateOfProcess(page, total_pages))
+
     return(res)
   }
 }
 
+# update the printed info
 printRateOfProcess <- function(page, total_pages){
   l = nchar(printed_info((page-1)/total_pages))
   str_sub(printed_info(page/total_pages), l+1, -1)
@@ -122,7 +139,7 @@ printRateOfProcess <- function(page, total_pages){
 #   cat(add_printed)
 # }
 
-
+# return the printed info response a updating ratio
 printed_info <- function(ratio){
   r1 = floor(100*ratio/20)
   r2 = floor((100*ratio-20*r1)/2)
@@ -143,15 +160,13 @@ printed_info <- function(ratio){
 # @usage rtSchema(...)
 rtSchema <- function(schemaclass = "Pathway",
                      species = "9606",
-                     specified = "detail", # detail simplified count or reference
-                     page = 1,
-                     offset = 25,
+                     type = "detail", # detail summary count or reference
+                     intervalOFpathway = c(1,10),
                      silent = FALSE){
   return(reactomeSchema(schemaclass = schemaclass,
-                        species = specified,
-                        specified = specified,
-                        page = page,
-                        offset = offset,
+                        species = species,
+                        type = type,
+                        intervalOFpathway = intervalOFpathway,
                         silent = silent))
 }
 
