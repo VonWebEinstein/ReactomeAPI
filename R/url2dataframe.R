52f5cf5a5a4055eb62cc4fd836d88844d78c422d
VonWebEinstein <vonweb@163.com>
2017-04-09 02:40:06 +0800

    mod url2dateframe

--- R/url2dataframe.R
+++ R/url2dataframe.R
@@ -10,17 +10,21 @@ url2dataframe <- function(urlComponent,
                           rework = function(s) s){
 
   url = str_c(urlComponent, collapse = '/')
-  res = try(fromJSON(url), silent = TRUE)
-  if(inherits(res, "try-error")){
-    tmp = content(GET(url, content_type_json()))
-    return(errorMessage(tmp, silent = silent))
-  }
-  else{
+  res = GET(url, content_type_json())
+  if(!res$status_code == 200)
+    return(errorMessage(content(res), silent = silent))
+  res = fromJSON(rawToChar(res$content))
+  # res = try(fromJSON(url), silent = TRUE)
+  # if(inherits(res, "try-error")){
+  #   tmp = content(GET(url, content_type_json()))
+  #   return(errorMessage(tmp, silent = silent))
+  # }
+  # else{
     # bind dataframes to single one
     if(bindToSingleDataframe && !is.data.frame(res)){
       res = do.call(rbind, res)
     }
     return(rework(res))
-  }
+
 
 }
