1524d92748aed1c8feb7e5cd39b3cb644e0c5b2f
VonWebEinstein <vonweb@163.com>
2017-04-09 02:39:12 +0800

    add bindbyrow

--- R/tools.R
+++ R/tools.R
@@ -4,3 +4,17 @@ validateAndCreatDir <- function(dir){
   if(!dir.exists(dir))
     dir.create(dir)
 }
+
+# bind two dataframes, won't check data type of the same column
+bindbyrow <- function(dt1, dt2){
+  ns1 = names(dt1)
+  ns2 = names(dt2)
+  KEYS = unique(c(ns1, ns2))
+  if(!all(sapply(c(ns1, ns2), length) == length(KEYS))){
+    missingNames = KEYS[!(KEYS %in% ns1)]
+    dt1[missingNames] = NA
+    missingNames = KEYS[!(KEYS %in% ns2)]
+    dt2[missingNames] = NA
+  }
+  rbind(dt1, dt2)
+}
